8866c1efaa7a20dfc4314eccd791989673de2728
Victor Delgado <valexdelgado@gmail.com>
2022-09-26 15:03:57 -0700

    finished

--- restSpread.js
+++ restSpread.js
@@ -9,12 +9,12 @@ function filterOutOdds() {
 Refactor it to use the rest operator & an arrow function:
 Write an ES2015 Version */
 
-const example = [1,2,3,4,5,6,7,8,9]
-function filterOutOdds(arr){
-    return [...arr].filter(num => num % 2 === 0)
+const example = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function filterOutOdds(arr) {
+  return [...arr].filter((num) => num % 2 === 0);
 }
 // or according to the solution (same result)
-const filterOutOddsFancy = (arr) => [...arr].filter(num => num % 2 === 0)
+const filterOutOddsFancy = (arr) => [...arr].filter((num) => num % 2 === 0);
 
 // console.log(filterOutOdds(example))
 // console.log(filterOutOddsFancy(example))
@@ -29,9 +29,9 @@ findMin(1,-1) // -1
 findMin(3,1) // 1
  */
 
-const findMin = (...args) => Math.min(...args)
+const findMin = (...args) => Math.min(...args);
 
-console.log(findMin(1,3,5,4,-5))
+console.log(findMin(1, 3, 5, 4, -5));
 
 /*
 Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
@@ -39,7 +39,7 @@ Write a function called mergeObjects that accepts two objects and returns a new 
 mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
  */
 
-const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
 
 // console.log(mergeObjects({a:1, b:2}, {c:3, d:4}))
 
@@ -50,7 +50,10 @@ Write a function called doubleAndReturnArgs which accepts an array and a variabl
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8] */
 
-const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map((elm) => elm*2)]
+const doubleAndReturnArgs = (arr, ...args) => [
+  ...arr,
+  ...args.map((elm) => elm * 2),
+];
 
 // console.log(doubleAndReturnArgs([1,2,3],4,4)) // [1,2,3,8,8]
 // console.log(doubleAndReturnArgs([2],10,4)) // [2, 20, 8]
@@ -62,16 +65,14 @@ For this section, write the following functions using rest, spread and refactor 
 Make sure that you are always returning a new array or object and not modifying the existing inputs.
 */
 
-
 /* remove a random element in the items array
 and return a new array without that item. */
 
-const itemsExample = [1,2,3,4,5]
+const itemsExample = [1, 2, 3, 4, 5];
 const removeRandom = (items) => {
-    let index = Math.floor(Math.random() * [...items].length)
-    return [...items.slice(0, index), ...items.slice(index + 1)]
-    
-}
+  let index = Math.floor(Math.random() * [...items].length);
+  return [...items.slice(0, index), ...items.slice(index + 1)];
+};
 
 // console.log(removeRandom(itemsExample))
 // console.log(removeRandom(itemsExample))
@@ -89,33 +90,22 @@ const removeRandom = (items) => {
 
 /** Return a new array with every item in array1 and array2. */
 
-const extend = (arr1, arr2) => [...arr1, ...arr2]
-console.log(extend([1,2], [3,4,5,1]))
+const extend = (arr1, arr2) => [...arr1, ...arr2];
+console.log(extend([1, 2], [3, 4, 5, 1]));
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {
-
-}
-
+const addKeyVal = (obj, key, val) => ({ ...obj, [key]: val });
 
 /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {
-
-}
-
+const removeKey = (obj, key) => ({ [key]: undefined, ...obj });
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {
-
-}
-
+const combine = (obj1, obj2) => ({ ...obj1, obj2 });
 
 /** Return a new object with a modified key and value. */
-
-function update(obj, key, val) {
-
-}
+    
+const update = (obj, key, val) => ( { ...obj, [key]: val });
